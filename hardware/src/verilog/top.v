16cb9ec2b86dfe357d1c28a1b64101a898bf992c
dwclake <clake.dw@gmail.com>
2024-01-13 20:50:59 -0800

    udpate verilog

--- hardware/src/verilog/top.v
+++ hardware/src/verilog/top.v
@@ -3,16 +3,20 @@
  */
 
 module top (
-    input sys_clk,
+    input  sys_clk,
+    input  enable,
+    input  spi_clk,
+    input  pico,
+    input  cs,
+    output data_out,
     output [7:0] led
 );
-
     reg [31:0] counter = 32'b0;
 
     assign led = counter[25:18];
 
+    /* Increment counter each clock tick */
     always @ (posedge sys_clk) begin
         counter <= counter + 1;
     end
-
 endmodule
